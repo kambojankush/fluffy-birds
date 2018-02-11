a4c9c511f4e2ae7b6b3a9dcdd9698efbe73bdf21
kambojankush <ankushkamboj06@gmail.com>
2018-02-11 15:56:14 +0530

    Parallax Update

--- main.lua
+++ main.lua
@@ -1,13 +1,27 @@
+-- virtual resolution handling library
 push = require 'push'
 
+-- physical screen dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+-- virtual resolution dimensions
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+-- background image and starting scroll location (X axis)
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+-- point at which we should loop our background back to X 0
+local BACKGROUND_LOOPING_POINT = 413
+
+-- ground image and starting scroll location (X axis)
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+-- speed at which we should scroll our images
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
 
 function love.load()
    love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -24,17 +38,26 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+function love.update(dt)
+    -- scroll background by preset speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    -- draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
+    -- draw the ground on top of the background, toward the bottom of the screen,
+    -- at its negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
 end
 
-function love.keypressed(key)
-    
+function love.keypressed(key)   
     if key == 'escape' then
         love.event.quit()
     end
